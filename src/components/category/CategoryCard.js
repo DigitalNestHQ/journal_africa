bc06b71ca458dfef1f84c0f8472e5ae3c687df7f
unclebay <unclebigbay@gmail.com>
2021-04-13 10:30:34 +0100

    fix responsiveness for category

--- src/components/category/CategoryCard.js
+++ src/components/category/CategoryCard.js
@@ -1,7 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ReactHtmlParser, {
+} from "react-html-parser";
+import { HtmlParseOptions } from "../../_helper/parseNewsHtml";
 
-const CategoryCard = ({ post_title, featured_image, id, post_type, slug }) => {
+const CategoryCard = ({ post_title, featured_image, id, post_type, slug, post_description }) => {
+  const getAllParagraphs = post_description.split("</p>")
+  const firstParagraph = getAllParagraphs[0];
+  let html;
+  if(post_description){
+    html = `${firstParagraph.slice(0,50)}...`
+  }
   return (
     <div className="main-section-card card-only sec-2">
       <div className="image category-img">
@@ -15,21 +24,27 @@ const CategoryCard = ({ post_title, featured_image, id, post_type, slug }) => {
       </div>
 
       <div className="card-caption mt-lg-0">
+      <Link to={`/post/${slug}`}>
         <h3 className="card-caption-title">
           {" "}
           {post_title.toLowerCase().slice(0, 100)}{" "}
         </h3>
+      </Link>
         {/* <br /> */}
-        <Link to={`/post/${slug}`}>
+        {/* <Link to={`/post/${slug}`}> */}
           <div  className="card-caption-content">
-            <p className="mb-1">
+            <p className="mb-1 mb-md-1 mb-lg-0">
               {" "}
               {/* {slug.toLowerCase().slice(0, 50)}... <br /> <span>Read more</span> */}
-              {slug.toLowerCase().slice(0, 50)}<br />
+              {/* {slug.toLowerCase().slice(0, 50)} */}
+              {ReactHtmlParser(html, HtmlParseOptions)}
+              {/* <br /> */}
+            <Link to={`/post/${slug}`}>
+              <span> Read more</span>
+            </Link>
             </p>
-              <span> Read more... </span>
           </div>
-        </Link>
+        {/* </Link> */}
       </div>
     </div>
   );

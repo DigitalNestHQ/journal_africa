3578c0e635917684ae9c0ed48b1f6879e00913a1
otutukingsley <chineduotutu3@gmail.com>
2022-02-11 00:30:04 -0800

    updated logo

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import './loader.css';
-import logoImg from 'assets/images/logo white.png';
+import React from "react"
+import "./loader.css"
+import logoImg from "assets/images/logo white.png"
+import newLogo from "assets/images/updatedLogo.jpeg"
 
 const Loader = () => {
   return (
     <div className="loader_wrapper">
       <div className="loader">
-        <img src={logoImg} alt="TV24Africa" className="loader-img"/>
+        <img src={newLogo} alt="TV24Africa" className="loader-img" />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Loader;
+export default Loader

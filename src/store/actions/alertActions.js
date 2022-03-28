6737f55c84ba3e578f65895373dac477bfd54d25
otutukingsley <chineduotutu3@gmail.com>
2022-03-28 07:43:10 -0700

    fixed alert and error messages

--- src/store/actions/alertActions.js
+++ src/store/actions/alertActions.js
@@ -8,6 +8,12 @@ export const showAlert = (message, type) => (dispatch) => {
     type: alertTypes.SET_ALERT,
     payload: { message, type, id },
   })
+
+  setTimeout(() => {
+    dispatch({ type: alertTypes.REMOVE_ALERT, payload: id })
+    dispatch({ type: userTypes.CLEAR_USER_MESSAGES })
+    dispatch({ type: userTypes.CLEAR_USER_ERRORS })
+  }, 10000)
 }
 
 export const removeAlert = (id) => (dispatch) => {

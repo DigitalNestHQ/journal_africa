b53550358bd9e34d49e04aa2bbfad344ce4ed1b4
Agba Emmanuel <agbaayoh@gmail.com>
2022-06-22 04:06:28 -0700

    podcast theme, radio and tv podcast removed

--- src/podcast/home/podcategories/PodcastCategories.js
+++ src/podcast/home/podcategories/PodcastCategories.js
@@ -1,37 +1,35 @@
-import React from "react"
-import { useHistory } from "react-router-dom"
-import { Link } from "react-router-dom"
-import play_button from "../../../assets/images/play_button.svg"
-import "./podnavbar.css"
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import play_button from '../../../assets/images/play_button.svg';
+import './podCategory.css';
 const PodcastCategories = ({ header, podcasts }) => {
-  const history = useHistory()
+  const history = useHistory();
   return (
-    <div className="each-pod-category">
-      <div className="pod-category-header">
-        <h5 className="section-heading-default-pod text-white pod-header">
-          {header}
-        </h5>
+    <div className='each-pod-category'>
+      <div className='pod-category-header'>
+        <h5 className='section-heading-default-pod pod-header'>{header}</h5>
       </div>
-      <div className="pod-category-list">
+      <div className='pod-category-list'>
         {podcasts.slice(0, podcasts.length).map((podcast) => (
-          <div key={podcast.collectionName} className="pod-collections">
-            <div className="pod-grid">
-              <div className="pod-img-container">
+          <div key={podcast.collectionName} className='pod-collections'>
+            <div className='pod-grid'>
+              <div className='pod-img-container'>
                 <img
                   src={podcast.artworkUrl600}
-                  alt="podcast-artwork"
-                  className="pod-img"
+                  alt='podcast-artwork'
+                  className='pod-img'
                 />
               </div>
-              <div className="pod-collection-details">
-                <div className="pod-intro-details">
-                  <p className="pod-text">
+              <div className='pod-collection-details'>
+                <div className='pod-intro-details'>
+                  <p className='pod-text'>
                     Damini Ebunoluwa Ogulu (born 2 july 1991), known
                     professionally as Burna Boy, is a Nigerian singer,
                     songwriter, rapper, and dancer. He is one of the biggest and
                     most successful African artists.
                   </p>
-                  <p className="pod-text">
+                  <p className='pod-text'>
                     He rose to prominence in 2012 after releasing "Like to
                     Party", the lead single from his debut studio album L.I.F.E
                     (2013). In 2017, Burna Boy signed with Bad Habit/Atlantic
@@ -39,18 +37,18 @@ const PodcastCategories = ({ header, podcasts }) => {
                     internationally.
                   </p>
                 </div>
-                <div className="pod-cta">
+                <div className='pod-cta'>
                   <button
-                    className="pod-btn play-button"
+                    className='pod-btn play-button'
                     onClick={() =>
                       history.push(`/podcast/${podcast.collectionId}`)
                     }
                   >
-                    <img src={play_button} alt="play" />
+                    <img src={play_button} alt='play' />
                     Play Latest episode
                   </button>
                   <button
-                    className="pod-btn pod-subscribe"
+                    className='pod-btn pod-subscribe'
                     onClick={() => history.push(`/subscribe`)}
                   >
                     Subscribe
@@ -60,7 +58,7 @@ const PodcastCategories = ({ header, podcasts }) => {
             </div>
             <Link
               to={`/podcast/${podcast.collectionId}`}
-              className="pod-episodes"
+              className='pod-episodes'
             >
               ALL EPISODES
             </Link>
@@ -68,7 +66,7 @@ const PodcastCategories = ({ header, podcasts }) => {
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default PodcastCategories
+export default PodcastCategories;

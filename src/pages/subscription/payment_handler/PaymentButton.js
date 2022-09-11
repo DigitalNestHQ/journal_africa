6f8eaa4d47ea83fb8528c949f43e0a232efac02f
Agba Emmanuel <agbaayoh@gmail.com>
2022-09-11 05:36:44 -0700

    paystack payment button refactored

--- src/pages/subscription/payment_handler/PaymentButton.js
+++ src/pages/subscription/payment_handler/PaymentButton.js
@@ -2,67 +2,57 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import '../subscribe.css';
 import { useState } from 'react';
-import { toast } from 'react-toastify';
 // import PaystackPop from '@paystack/inline-js';
 import { usePaystackPayment } from 'react-paystack';
 
 export default function PaymentButton({ packageID, amount, packageName }) {
-  // const [loading, setLoading] = useState(false);
   const loginUser = useSelector((state) => state.loginUser);
   const { user } = loginUser;
 
   const config = {
     reference: new Date().getTime().toString(),
-    email: '',
+    email: 'agbaayoh@gmail.com',
     amount: 20000,
-    publicKey: '',
+    publicKey: 'pk_live_80458517341e0e85780177e5acee2f1014f264ec',
   };
 
   const onSuccess = (reference) => {
-    // Implementation for whatever you want to do with reference and after success call.
     console.log(reference);
   };
 
   const onClose = () => {
-    // implementation for  whatever you want to do when the Paystack dialog closed.
     console.log('closed');
   };
 
   const initializePayment = usePaystackPayment(config);
 
-  function subscribe() {
-    let handler = window.PaystackPop.setup({
-      key: '',
-      email: '',
-      amount: 1900000,
-      onClose: function () {
-        alert('Window closed.');
-        console.log('pay made');
-      },
-      callback: function (response) {
-        let message = 'Payment complete! Reference: ' + response.reference;
-        alert(message);
-        console.log('pay made', message);
-      },
-    });
-    handler.openIframe();
-  }
+  // function subscribe() {
+  //   let handler = window.PaystackPop.setup({
+  //     key: '',
+  //     email: '',
+  //     amount: 1900000,
+  //     onClose: function () {
+  //       alert('Window closed.');
+  //       console.log('pay made');
+  //     },
+  //     callback: function (response) {
+  //       let message = 'Payment complete! Reference: ' + response.reference;
+  //       alert(message);
+  //       console.log('pay made', message);
+  //     },
+  //   });
+  //   handler.openIframe();
+  // }
 
   return (
     <button
       className='subscription-btn'
       onClick={() => {
-        // initializePayment(onSuccess, onClose);
-        subscribe();
+        initializePayment(onSuccess, onClose);
+        // subscribe();
       }}
     >
-      {/* {loading ? (
-        <span>subscribing, please wait</span>
-      ) : (
-        <span>Click to Subscribe</span>
-        )} */}
       <span>Click to Subscribe</span>
-      {/* <PaystackButton {...componentProps} /> */}
     </button>
   );
 }

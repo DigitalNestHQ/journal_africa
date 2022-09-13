8fe3787c74be8dcfa95d107677cdb34ea550d01b
Agba Emmanuel <agbaayoh@gmail.com>
2022-09-13 08:59:11 -0700

    my tv journal africa logo changed

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -1,15 +1,15 @@
-import React from "react"
-import "./loader.css"
-import newLogo from "assets/images/main logo.png"
+import React from 'react';
+import './loader.css';
+import newLogo from 'assets/images/tv-journal-logo.png';
 
 const Loader = () => {
   return (
-    <div className="loader_wrapper">
-      <div className="loader">
-        <img src={newLogo} alt="TV24Africa" className="loader-img" />
+    <div className='loader_wrapper'>
+      <div className='loader'>
+        <img src={newLogo} alt='TV24Africa' className='loader-img' />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Loader
+export default Loader;
